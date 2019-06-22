988c199e6ec33c57afad8e610735ecd78e1b19c0
Cibele Yumi <contato@cibeleyumi.com.br>
2019-06-21 22:53:26 -0300

    feat: create reload actual page

--- scripts/_nav/navegacao.js
+++ scripts/_nav/navegacao.js
@@ -1,12 +1,16 @@
 import * as storagePaginaInicial from '/scripts/_storage/paginaInicial.js';
+import { formataEndereco } from '/scripts/_utils/formataEndereco.js';
 
 const paginaAtual = localStorage.getItem('paginaAtual');
 
 if(paginaAtual != null){
-  const enderecoCompleto = formataEndereco(storagePaginaInicial.paginaAtual);
+  const enderecoCompleto = formataEndereco(paginaAtual);
 
   $janelaPrincipal.src = enderecoCompleto;
   $inputEndereco.value = enderecoCompleto;
 } else {
-  const enderecoCompleto = formataEndereco
+  const enderecoCompleto = formataEndereco(storagePaginaInicial.paginaInicial);
+
+  $janelaPrincipal.src = enderecoCompleto;
+  $inputEndereco.value = enderecoCompleto;
 }
--- scripts/_pedeInfos/pedePaginaInicial.js
+++ scripts/_pedeInfos/pedePaginaInicial.js
@@ -32,23 +32,7 @@ if(storageAceitouSalvar === null || storageAceitouSalvar === true) {
 	}
 	
 	if(paginaInicialDefault) {
-
-		const paginaAtual = localStorage.getItem('paginaAtual');
-
-		if(paginaAtual != null){
-			const enderecoCompleto = formataEndereco(paginaAtual);
-
-			$janelaPrincipal.src = enderecoCompleto;
-			$inputEndereco.value = enderecoCompleto;
-		} else {
-			const enderecoCompleto = formataEndereco
-		}
-		
 		const enderecoCompleto = formataEndereco(paginaInicialDefault);
-	
-		$janelaPrincipal.src = enderecoCompleto;
-		$inputEndereco.value = enderecoCompleto;
-
 		storagePaginaInicial.setPaginaInicial(enderecoCompleto);
 	}
 }
